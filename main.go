f3b0286fe0a0696b37dcaefd468e999375068c40
Fastiraz <92016718+Fastiraz@users.noreply.github.com>
2023-12-21 22:27:14 +0100

    Update main.go

--- main.go
+++ main.go
@@ -24,7 +24,16 @@ func main() {
 				github.Email(argv[i+1])
 				os.Exit(0)
 			case "-d", "--discord":
+				discord.Gathering(argv[i+1])
+				os.Exit(0)
+			case "-dt", "--discordtoken":
 				discord.Token(argv[i+1])
+				os.Exit(0)
+			case "-df", "--discordfriends":
+				discord.Friend(argv[i+1])
+				os.Exit(0)
+			case "-dd", "--discorddata":
+				discord.UserData(argv[i+1])
 				os.Exit(0)
 			case "-@", "--email":
 				email.Email(argv[i+1])
@@ -37,13 +46,16 @@ func main() {
 					GOSInt is an OSINT tool.
 
 					FLAGS:
-						-h, --help    : Display this message
-						-n, --name    : Search for a name
-						-p, --phone   : Phone number lookup (dev)
-						-g, --github  : GitHub email lookup
-						-d, --discord : Discord Token Recovery
-						-@, --email   : Find where an email is registered (dev)
-						-c, --card	  : Display credit card information
+						-h, --help    			: Display this message
+						-n, --name    			: Search for a name
+						-p, --phone   			: Phone number lookup (dev)
+						-g, --github  			: GitHub email lookup
+						-d, --discord 			: Discord information gathering 
+						-dt, --discordtoken 	: Discord Token Recovery
+						-df, --discordfriends 	: target's Discord friends
+						-dd, --discorddata		: Discord account information
+						-@, --email   			: Find where an email is registered (dev)
+						-c, --card	  			: Display credit card information
 
 					EXAMPLE:
 						gosint -n <username>
